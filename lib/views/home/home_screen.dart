7393a822ca64cada844ea5fbd7cbfea67720e462
Afaq Zahir <116549225+MetasenseDevelopment@users.noreply.github.com>
2024-09-10 00:26:45 +0500

    done

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -22,7 +22,9 @@ class _HomeScreenState extends State<HomeScreen> {
     _scrollController = ScrollController()
       ..addListener(() {
         if (_scrollController.position.pixels ==
-            _scrollController.position.maxScrollExtent) {}
+            _scrollController.position.maxScrollExtent) {
+          // Add logic for pagination or additional fetching here
+        }
       });
 
     movieBloc.add(MovieFetchEvent()); // Initial fetch
@@ -36,151 +38,178 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print("im doing my job");
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Text("Home Page"),
       ),
-      body: BlocProvider(
-        create: (_) => movieBloc..add(MovieFetchEvent()),
-        child: BlocBuilder<MovieBloc, MovieState>(
-          builder: (context, state) {
-            final status = state.movieList.status;
+      body: Column(
+        children: [
+          NewWidget(),
+          BlocProvider(
+            create: (_) => movieBloc..add(MovieFetchEvent()),
+            child: BlocBuilder<MovieBloc, MovieState>(
+              builder: (context, state) {
+                final status = state.movieList.status;
 
-            if (status == null) {
-              return const Center(
-                child: Text(
-                  "Movie List is null",
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
-              );
-            }
-
-            switch (status) {
-              case ApiResponseStatus.loading:
-                return const Center(child: CircularProgressIndicator());
-
-              case ApiResponseStatus.error:
-                return Center(
-                  child: Text(state.movieList.message ?? "Unknown error"),
-                );
-
-              case ApiResponseStatus.complete:
-                if (state.movieList.data == null ||
-                    state.movieList.data!.tvShow.isEmpty) {
+                if (status == null) {
                   return const Center(
                     child: Text(
-                      "No Data",
+                      "Movie List is null",
                       style: TextStyle(
                           fontSize: 16.0, fontWeight: FontWeight.bold),
                     ),
                   );
                 }
 
-                final list = state.movieList.data!.tvShow;
-                return Column(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GridView.builder(
-                          controller: _scrollController,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2, // Number of columns in the grid
-                            crossAxisSpacing: 10.0, // Spacing between columns
-                            mainAxisSpacing: 10.0, // Spacing between rows
-                            childAspectRatio: 0.75, // Aspect ratio of each item
-                          ),
-                          itemCount: list.length,
-                          itemBuilder: (context, index) {
-                            final tvShow = list[index];
-                            return Container(
-                              decoration: BoxDecoration(
-                                color: Colors.black38,
-                                borderRadius: BorderRadius.circular(12),
+                switch (status) {
+                  case ApiResponseStatus.loading:
+                    return const Center(child: CircularProgressIndicator());
+
+                  case ApiResponseStatus.error:
+                    return Center(
+                      child: Text(state.movieList.message ?? "Unknown error"),
+                    );
+
+                  case ApiResponseStatus.complete:
+                    if (state.movieList.data == null ||
+                        state.movieList.data!.tvShow.isEmpty) {
+                      return const Center(
+                        child: Text(
+                          "No Data",
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    }
+
+                    final list = state.movieList.data!.tvShow;
+                    return Expanded(
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount:
+                                    2, // Number of columns in the grid
+                                crossAxisSpacing:
+                                    10.0, // Spacing between columns
+                                mainAxisSpacing: 10.0, // Spacing between rows
+                                childAspectRatio:
+                                    0.75, // Aspect ratio of each item
                               ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Container(
-                                    height: 210,
-                                    decoration: BoxDecoration(
-                                        borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(12),
-                                            topRight: Radius.circular(12)),
-                                        image: DecorationImage(
+                              itemCount: list.length,
+                              itemBuilder: (context, index) {
+                                final tvShow = list[index];
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.black38,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: 210,
+                                        decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.only(
+                                              topLeft: Radius.circular(12),
+                                              topRight: Radius.circular(12)),
+                                          image: DecorationImage(
                                             fit: BoxFit.fill,
                                             image: NetworkImage(
                                               tvShow.imageThumbnailPath,
-                                            ))),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          tvShow.name,
+                                          style: const TextStyle(
+                                              fontSize: 16.0,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      tvShow.name,
-                                      style: const TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    Container(
-                      color: Colors.white,
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: List.generate(
-                          15,
-                          (index) {
-                            int pageIndex = index + 1;
-                            return GestureDetector(
-                              onTap: () {
-                                context
-                                    .read<MovieBloc>()
-                                    .add(MovieFetchEvent(page: pageIndex));
+                                );
                               },
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 4.0),
-                                child: Text(
-                                  pageIndex.toString(),
-                                  style: TextStyle(
-                                    fontSize: pageIndex == movieBloc.currentPage
-                                        ? 20.0
-                                        : 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: pageIndex == movieBloc.currentPage
-                                        ? Colors.red
-                                        : Colors.black,
-                                  ),
-                                ),
+                            ),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: List.generate(
+                                15,
+                                (index) {
+                                  int pageIndex = index + 1;
+                                  return GestureDetector(
+                                    onTap: () {
+                                      context.read<MovieBloc>().add(
+                                          MovieFetchEvent(page: pageIndex));
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 4.0),
+                                      child: Text(
+                                        pageIndex.toString(),
+                                        style: TextStyle(
+                                          fontSize:
+                                              pageIndex == movieBloc.currentPage
+                                                  ? 20.0
+                                                  : 16,
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              pageIndex == movieBloc.currentPage
+                                                  ? Colors.red
+                                                  : Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
                               ),
-                            );
-                          },
-                        ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                );
+                    );
 
-              default:
-                return const Text(
-                  "Unknown state",
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                );
-            }
-          },
-        ),
+                  default:
+                    return const Text(
+                      "Unknown state",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    );
+                }
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print("i am build ");
+    return Container(
+      height: 199,
+      width: 300,
+      color: Colors.amber,
     );
   }
 }
